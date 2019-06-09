43660a9b9abf7ce27b68f43e621e79ff39b08bfb
timofun <timo_quiet@163.com>
2019-06-09 10:34:23 +0800

    update 10:34

--- src/components/platform/approval-flow/flow-config/field-set/node-sort.vue
+++ src/components/platform/approval-flow/flow-config/field-set/node-sort.vue
@@ -11,45 +11,46 @@
         :append-to-body="true"
         :close-on-click-modal="false"
         :show-close="false"
-        :visible="true"   
+        :visible="true" 
       >
         <!-- <div class="showTit">提示：不建议改变领域名，会影响相关数据。即使更改也请保持同原业务领域类别保持一致，仅对名称进行重命名。</div> -->
-        <div class="TipBox">
-          <span class="sortTipTit">拖拽排序</span>
-        </div>
-        <!-- {{tableData_dialog}} -->
-        <vuedraggable class="wrapper" v-model="tableData_dialog"  :options = "{animation:500}">
-            <transition-group>
-              <div  v-for="(obj, index) in tableData_dialog" :key="index+1" class="itemBox">
-                    <el-menu
-                      default-active="2"
-                      class="el-menu-vertical-demo"
-                    >
-                      <el-menu-item index="2">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">{{obj.Name}}  ({{obj.NodePosTypeText}})</span>
-                      </el-menu-item>
-                    </el-menu>
-                <!-- <el-input 
-                  v-show=" editTeamNameIndex!=-1 && index == editTeamNameIndex"
-                  class="sort-cart-item"
-                  v-model="rename"
-                >
-                </el-input> -->
-                <!-- <div class="editAndDelet">
-                  <span class="edit" v-show="(index != editTeamNameIndex)" @click="handleEditTeam(index)">编辑</span>
-                  <span class=" EditCancel" v-show="(index == editTeamNameIndex)" @click="editTeamNameIndex = -1">取消</span>
-                  <span class=" EditSave" v-show="(index == editTeamNameIndex)" @click="handleSaveTeamName">保存</span>
-                </div> -->
-              </div>
-            </transition-group>              
-        </vuedraggable>
+        <div class="containerBox" v-loading="sortLoading">
+          <div class="TipBox">
+            <span class="sortTipTit">拖拽排序</span>
+          </div>
+          <!-- {{tableData_dialog}} -->
+          <vuedraggable class="wrapper" v-model="tableData_dialog"  :options = "{animation:500}">
+              <transition-group>
+                <div  v-for="(obj, index) in tableData_dialog" :key="index+1" class="itemBox">
+                      <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo"
+                      >
+                        <el-menu-item index="2">
+                          <i class="el-icon-menu"></i>
+                          <span slot="title">{{obj.Name}}  ({{obj.NodePosTypeText}})</span>
+                        </el-menu-item>
+                      </el-menu>
+                  <!-- <el-input 
+                    v-show=" editTeamNameIndex!=-1 && index == editTeamNameIndex"
+                    class="sort-cart-item"
+                    v-model="rename"
+                  >
+                  </el-input> -->
+                  <!-- <div class="editAndDelet">
+                    <span class="edit" v-show="(index != editTeamNameIndex)" @click="handleEditTeam(index)">编辑</span>
+                    <span class=" EditCancel" v-show="(index == editTeamNameIndex)" @click="editTeamNameIndex = -1">取消</span>
+                    <span class=" EditSave" v-show="(index == editTeamNameIndex)" @click="handleSaveTeamName">保存</span>
+                  </div> -->
+                </div>
+              </transition-group>              
+          </vuedraggable>
 
-        <div class="dialog-footer" style="display: block; padding: 10px 0; text-align: center">
-          <el-button @click.native="cancelSort()">取 消</el-button>
-          <el-button type="primary" @click.native="SaveSortNode()">保存</el-button>
+          <div class="dialog-footer" style="display: block; padding: 10px 0; text-align: center">
+            <el-button @click.native="cancelSort()">取 消</el-button>
+            <el-button type="primary" @click.native="SaveSortNode()">保存</el-button>
+          </div>
         </div>
-
       </el-dialog>  
 </template>
 
@@ -75,7 +76,8 @@
     data () {
       return {
         tableData_dialog: this.tableData,
-        dialogSortNode_dialog: this.dialogSortNode
+        dialogSortNode_dialog: this.dialogSortNode,
+        sortLoading: false
       }
     },
     created () {
@@ -116,8 +118,9 @@
         this.tableData.forEach((item, idx) => {
           item.step = ++idx
         })
-  
+        this.sortLoading = true
         SortNode(JSON.stringify(this.tableData)).then((res) => {
+          this.sortLoading = false
           if (res && res.data.State === REQ_OK) {
             debugger
             this.$message({
@@ -127,9 +130,11 @@
             this.dialogSortNode_dialog = false
             // this.$emit('update:dialogSortNode', false)
           } else {
+            this.sortLoading = false
             this.$message.error(`排序失败err,请刷新后重试`)
           }
         }).catch(() => {
+          this.sortLoading = false
           this.$message.error(`排序失败err,请刷新后重试`)
         })
       },
@@ -145,4 +150,6 @@
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 >>>.el-dialog__body
   padding 10px 20px !important
+  .el-loading-mask
+    top -50px !important
 </style>
