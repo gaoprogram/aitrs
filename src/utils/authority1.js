15333cf347d1aee093bb1d9acea647ff4d41dfdb
gaol <331319344@qq.com>
2020-06-08 21:07:30 +0800

    update

--- src/utils/authority1.js
+++ src/utils/authority1.js
@@ -1,19 +1,19 @@
 export const authorityArr = [{
 		"pageCode": "page1",
 		"EleStyle": 8,
-		"parent": "root",
+		"parentCode": "root",
 		"sub": [{
 			"pageCode": "com1",
 			"EleStyle": 8,
-			"parent": "page1",
+			"parentCode": "page1",
 			"sub": [{
 				"pageCode": "com3",
 				"EleStyle": 8,
-				"parent": "com1",
+				"parentCode": "com1",
 				"sub": [{
 					"pageCode": "btn1",
 					"EleStyle": 1,
-					"parent": "com1"
+					"parentCode": "com1"
 				}]
 			}]
 		}]
@@ -22,15 +22,15 @@ export const authorityArr = [{
 
 		"pageCode": "page2",
 		"EleStyle": 8,
-		"parent": "root",
+		"parentCode": "root",
 		"sub": [{
 			"pageCode": "com1",
 			"EleStyle": 8,
-			"parent": "page2",
+			"parentCode": "page2",
 			"sub": [{
 				"pageCode": "com3",
 				"EleStyle": 8,
-				"parent": "com1",
+				"parentCode": "com1",
 				"sub": [
 
 				]
